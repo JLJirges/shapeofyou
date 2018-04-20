fe9ae250c7b524fbe25be7ae56f9e050b73ba802
JJirges <jeanne.jirges@yahoo.at>
2018-04-20 20:23:29 +0200

    Add first view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,94 +2,139 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title>Shape of you!</title>
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+    <div class="flex-center position-ref full-height">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <div class="top-middle links">
+            <a href="http://localhost/shapeofyou/resources/views/superfood.blade.php">Super Food</a>
+            <a href="http://localhost/shapeofyou/resources/views/workout.blade.php">Workout</a>
+            <a href="http://localhost/shapeofyou/resources/views/community.blade.php">Community</a>
+            <a href="http://localhost/shapeofyou/resources/views/welcome.blade.php">
+                <img class="logo_header" src="{{ asset('images/headerfooter/Logo.png') }}" alt="logo">
+            </a>
+            <a href="http://localhost/shapeofyou/resources/views/meetups.blade.php">Meetups</a>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <a href="#">Login</a>
+            <a href="http://localhost/shapeofyou/resources/views/register.blade.php">Start now</a>
+
+        </div>
+
+
+        <div class="content">
+            <!-- <div class="heroimg">
+
+             </div> -->
+
+            <h2> SHAPE OF YOU</h2>
+
+            <div class="home_section goal_bg">
+                <div>
+                    <h3>Choose your goal!</h3>
+                    <div class="home_section_goals">
+                        <img alt="lose weight" src="{{ asset('images/lose_weight_3.png') }}" width="120" height="110">
+                        <img alt="become fit" src="{{ asset('images/become_fit_2.png') }}" width="120" height="110">
+                        <img alt="lose weight" src="{{ asset('images/build_muscles_2.png') }}" width="120" height="110">
+                        <img alt="become fit" src="{{ asset('images/healthy_lifestyle_2.png') }}" width="120" height="110">
+                        <!-- <a href="https://laravel.com/docs">Documentation</a>
+                        <a href="https://laracasts.com">Laracasts</a>
+                        <a href="https://laravel-news.com">News</a>
+                        <a href="https://forge.laravel.com">Forge</a>
+                        <a href="https://github.com/laravel/laravel">GitHub</a> -->
+                    </div>
+                    <div class="goal_info">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam architecto dolor error
+                            incidunt ipsum itaque, iure, iusto non obcaecati officiis, quas quo repudiandae vel velit? Nobis
+                            obcaecati tempore velit!
+                        </p>
+
+                    </div>
                 </div>
             </div>
+
+
         </div>
+
+
+    </div>
+
+    <div class="home_section home_section_community">
+        <div>
+            <h3>Community</h3>
+            <p>
+                Are you ready to become part of Austria's biggest lifestyle event? Choose your goal and achieve it. Alone or
+                togehter - you decide. Become part of the community and get tips, tricks, the newest workouts and motivation
+                from people all around Austria! Join the big events and have real talks to our fitness professionals, who
+                are always there to help you out - online and in person!
+                What do you think? We think, you should:
+            </p>
+            <a href="#">START NOW</a>
+        </div>
+    </div>
+
+    <div class="home_section shape_bg">
+        <div>
+            <h3>Choose Your Body Shape</h3>
+            <div class="home_body_shape_img">
+                <img alt="hour glass shape" src="{{ asset('images/hourglass_shape_1.png') }}" width="120" height="120">
+                <img alt="apple shape" src="{{ asset('images/apple_shape_1.png') }}" width="120" height="120">
+                <img alt="pear shape" src="{{ asset('images/pear_shape_1.png') }}" width="120" height="120">
+                <img alt="stick_shape" src="{{ asset('images/stick_shape_1.png') }}" width="120" height="120">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="home_section home_section_food">
+            <div>
+                <h3>Super Food</h3>
+                <p>
+                    Are you ready to become part of Austria's biggest lifestyle event? Choose your goal and achieve it. Alone or
+                    togehter - you decide. Become part of the community and get tips, tricks, the newest workouts and motivation
+                    from people all around Austria! Join the big events and have real talks to our fitness professionals, who
+                    are always there to help you out - online and in person!
+                    What do you think? We think, you should:
+                </p>
+                <a href="#">START NOW</a>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer_content footer_content_1">
+            <div class="footer_options">
+                <a href="#">About us</a>
+                <a href="#">Jobs</a>
+                <a href="#">Contact</a>
+            </div>
+
+            <div class="footer_options">
+                <a href="#">Become a Member</a>
+                <a href="#">Terms and Conditions</a>
+                <a href="#">FAQ</a>
+            </div>
+        </div>
+        <div class="footer_content footer_content_2">
+            <div class="footer_follow">
+                <span>Follow us on</span>
+            </div>
+            <div class="footer_follow footer_follow_images">
+                <a href="#"><img alt="share instagram" src="{{ asset('images/headerfooter/share_instagram.png') }}"></a>
+                <a href="#"><img alt="share instagram" src="{{ asset('images/headerfooter/share_facebook.png') }}"></a>
+            </div>
+            <div class="follow_footer">
+                <span>
+                    © by J.L. Jirges
+                </span>
+            </div>
+        </div>
+    </footer>
     </body>
 </html>
