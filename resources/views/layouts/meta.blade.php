6469c4f175205b7caba70d0c68468ac83ddc2030
JJirges <jeanne.jirges@yahoo.at>
2018-07-02 21:47:39 +0200

    change profile layout

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -2,5 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="{{ asset('images/headerfooter/Logo.png') }}">
-<link href="{{url('https://fonts.googleapis.com/css?family=Laila')}}" rel="stylesheet">
+<link href="{{url('https://fonts.googleapis.com/css?family=Railway')}}" rel="stylesheet">
 
