68bf98c7f74b3c6b975b7a09e8b07a4eaed95819
JJirges <jeanne.jirges@yahoo.at>
2018-07-02 22:07:45 +0200

    Change Superefood to Blogs.Bring Workout Blogs to Blogs. Add css effect to Blogs.

--- resources/views/superfood.blade.php
+++ resources/views/superfood.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.master')
 
 @section('title')
-    <title>SHAPE OF YOU | Superfood</title>
+    <title>SHAPE OF YOU | Blogs</title>
 @endsection
 
 @section('content')
     <div class="h1_bg">
-        <h1>SUPER FOOD</h1>
+        <h1>BLOGS</h1>
     </div>
     <p class="superfood_text">Are you hungry or just looking for inspiration? Then this is the place to be!
         Choose the occasion, decide the diet or just go for it all! We are here to help you
@@ -24,7 +24,7 @@
             <div class="superfood_blog_bg">
                 <div class="square_box_section">
                     @foreach($blogs->where('BlogCategory', 1) as $blog)
-                        <div style="background-image:url({{'images/blogs/' . $blog->BlogHeroImage}});background-size:cover; background-position:center;">
+                        <div class="slide-in-elliptic-top-fwd" style="background-image:url({{'images/blogs/' . $blog->BlogHeroImage}});background-size:cover; background-position:center;">
                             <a class="box_link"
                                href="@if( (auth()->check())){{url('blog/' . $blog->id)}}@else{{ url ('register') }}@endif">
                                 {{$blog->BlogTitle}}
@@ -42,8 +42,6 @@
             <div class="title_bg">
                 <h2>Diet Blogs</h2>
             </div>
-
-
             <div class="superfood_blog_bg">
                 <div class="square_box_section">
                     @foreach($blogs->where('BlogCategory', 2) as $blog)
@@ -60,6 +58,26 @@
 
             @endif
 
+    <div class="title_bg">
+        <h2>Workout Blogs</h2>
+    </div>
+    @if($blogs->where('BlogCategory', 4)->count() > 0)
+        <div class="workout_blog_bg">
+
+
+            <div class="square_box_section">
+                @foreach($blogs->where('BlogCategory', 4) as $blog)
+                    <div style="background-image:url({{'images/blogs/' . $blog->BlogHeroImage}});background-size:cover; background-position:center;">
+                        <a class="box_link"
+                           href="@if( (auth()->check())){{url('blog/' . $blog->id)}}@else{{ url ('register') }}@endif">
+                            {{$blog->BlogTitle}}  </a>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
+
+    @endif
 
 
         <div class="title_bg">
--- resources/views/workout.blade.php
+++ resources/views/workout.blade.php
@@ -39,33 +39,35 @@
                                     <text x="28" text-anchor="middle" y="34" dy="0">P</text>
                                 </mask>
                             </defs>
-                            <rect x="0.5" y="0.5" width="220" height="215" mask="url(#warmup_mask)" fill-opacity="1" fill="rgba(255, 255, 255, 0.9)"/>
+                            <rect x="0.5" y="0.5" width="220" height="215" mask="url(#warmup_mask)" fill-opacity="1"
+                                  fill="rgba(255, 255, 255, 0.9)"/>
                         </svg>
                     </a>
                 </div>
             @endif
-                @if($workout->where('WorkoutCategory', 2)->count() > 0)
-                    <div class="box_stretch">
-                        <a href="@if( (auth()->check())){{url('overview/2')}}@else{{ url ('register') }}@endif">
-                            <svg viewbox="0 -1 50 47">
-                                <defs>
-                                    <mask class="mask" id="stretch_mask" x="0" y="0" width="220" height="215">
-                                        <rect x="0.5" y="0.5" width="49" height="49" fill="#fff"/>
-                                        <text x="6" text-anchor="middle" y="30" dy="0">S</text>
-                                        <text x="12" text-anchor="middle" y="30" dy="0">T</text>
-                                        <text x="18" text-anchor="middle" y="30" dy="0">R</text>
-                                        <text x="24" text-anchor="middle" y="30" dy="0">E</text>
-                                        <text x="30" text-anchor="middle" y="30" dy="0">T</text>
-                                        <text x="37" text-anchor="middle" y="30" dy="0">C</text>
-                                        <text x="44" text-anchor="middle" y="30" dy="0">H</text>
-                                    </mask>
-                                </defs>
-                                <rect x="0.5" y="0.5" width="220" height="215" mask="url(#stretch_mask)" fill-opacity="1" fill="rgba(000, 000, 000, 0.9)"/>
-                            </svg>
-                        </a>
-                    </div>
-                @endif
-                @if($workout->where('WorkoutCategory', 3)->count() > 0)
+            @if($workout->where('WorkoutCategory', 2)->count() > 0)
+                <div class="box_stretch">
+                    <a href="@if( (auth()->check())){{url('overview/2')}}@else{{ url ('register') }}@endif">
+                        <svg viewbox="0 -1 50 47">
+                            <defs>
+                                <mask class="mask" id="stretch_mask" x="0" y="0" width="220" height="215">
+                                    <rect x="0.5" y="0.5" width="49" height="49" fill="#fff"/>
+                                    <text x="6" text-anchor="middle" y="30" dy="0">S</text>
+                                    <text x="12" text-anchor="middle" y="30" dy="0">T</text>
+                                    <text x="18" text-anchor="middle" y="30" dy="0">R</text>
+                                    <text x="24" text-anchor="middle" y="30" dy="0">E</text>
+                                    <text x="30" text-anchor="middle" y="30" dy="0">T</text>
+                                    <text x="37" text-anchor="middle" y="30" dy="0">C</text>
+                                    <text x="44" text-anchor="middle" y="30" dy="0">H</text>
+                                </mask>
+                            </defs>
+                            <rect x="0.5" y="0.5" width="220" height="215" mask="url(#stretch_mask)" fill-opacity="1"
+                                  fill="rgba(000, 000, 000, 0.9)"/>
+                        </svg>
+                    </a>
+                </div>
+            @endif
+            @if($workout->where('WorkoutCategory', 3)->count() > 0)
                 <div class="box_indoor">
                     <a href="@if( (auth()->check())){{url('overview/3')}}@else{{ url ('register') }}@endif">
                         <svg viewbox="0 -1 50 47">
@@ -87,39 +89,40 @@
                                     <text x="46" text-anchor="middle" y="35" dy="0">T</text>
                                 </mask>
                             </defs>
-                            <rect x="0.5" y="0.5" width="220" height="205" mask="url(#mainworkout_mask)" fill-opacity="1" fill="rgba(255, 255, 255, 0.9)"/>
+                            <rect x="0.5" y="0.5" width="220" height="205" mask="url(#mainworkout_mask)"
+                                  fill-opacity="1" fill="rgba(255, 255, 255, 0.9)"/>
                         </svg>
 
                     </a>
                 </div>
-                @endif
+            @endif
             @if($workout->where('WorkoutCategory', 4)->count() > 0)
                 <div class="box_tenminutes">
                     <a class="box_link"
                        href="@if( (auth()->check())){{url('overview/4')}}@else{{ url ('register') }}@endif">10 MINUTES
                         WORKOUT</a>
                 </div>
-                @endif
+            @endif
             @if($workout->where('WorkoutCategory', 5)->count() > 0)
                 <div class="box_partnerworkout">
                     <a class="box_link"
                        href="@if( (auth()->check())){{url('overview/5')}}@else{{ url ('register') }}@endif">PARTNER
                         WORKOUT</a>
                 </div>
-                @endif
+            @endif
             @if($workout->where('WorkoutCategory', 6)->count() > 0)
                 <div class="box_yoga">
                     <a class="box_link"
                        href="@if( (auth()->check())){{url('overview/6')}}@else{{ url ('register') }}@endif">YOGA</a>
                 </div>
-                @endif
+            @endif
             @if($workout->where('WorkoutCategory', 7)->count() > 0)
                 <div class="box_workoutwithequipment">
                     <a class="box_link"
                        href="@if( (auth()->check())){{url('overview/7')}}@else{{ url ('register') }}@endif">POWER WITH
                         EQUIPMENT</a>
                 </div>
-                @endif
+            @endif
             @if($workout->where('WorkoutCategory', 8)->count() > 0)
                 <div class="box_outdoor">
                     <a class="box_link"
@@ -130,26 +133,7 @@
 
     </div>
 
-    @if($blogs->where('BlogCategory', 4)->count() > 0)
-        <div class="title_bg">
-            <h2>Workout and Training Blogs</h2>
-        </div>
-    <div class="workout_blog_bg">
 
-
-        <div class="square_box_section">
-            @foreach($blogs->where('BlogCategory', 4) as $blog)
-                <div style="background-image:url({{'images/blogs/' . $blog->BlogHeroImage}});background-size:cover; background-position:center;">
-                    <a class="box_link"
-                       href="@if( (auth()->check())){{url('blog/' . $blog->id)}}@else{{ url ('register') }}@endif">
-                        {{$blog->BlogTitle}}  </a>
-                </div>
-            @endforeach
-        </div>
-
-    </div>
-
-    @endif
 
 
 @endsection
