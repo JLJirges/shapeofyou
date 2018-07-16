8fc807e87bf822bd2250e3a0b762e9f641daf248
JJirges <jeanne.jirges@yahoo.at>
2018-07-16 16:09:46 +0200

    fix css issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,20 +11,15 @@
     </div>
 
     <div class="home_section goal_bg">
-        <div>
+        <h2 class="slogan">WE HELP YOU SHAPE YOUR BODY</h2>
+        <div class="home_section_goals_content">
             <h2>Choose your goal!</h2>
-            <div class="home_section_goals_content shown_tab">Whatever your personal goal might be - we are here to help
+            <div class="shown_tab">Whatever your personal goal might be - we are here to help
                 you! Lose weight, improve stamina,
                 build muscles or find your way to a healthy/healthier lifestyle.
                 It is your desicion, your goal, your achievements -
                 SHAPE OF YOU!
             </div>
-
-
-
-
-
-
             <ul class="tabs home_section_goals">
                 <li class="tab-link current_tab" data-tab="goal-tab-1">
                     <img alt="lose weight" src="{{ asset('images/lose_weight_3.png') }}" width="110" height="110">
@@ -41,26 +36,28 @@
             </ul>
 
             <div id="goal-tab-1" class="tab-content current_tab">
-                If you want to lose weight, we are going to help! You'll find a lot of interesting Blogs about
+                <strong>WEIGHT LOSS</strong> We are going to help! You'll find a lot of interesting Blogs about
                 Nutrition and Workout, so you are going to feel as awesome as possible with SHAPE OF YOU!
             </div>
             <div id="goal-tab-2" class="tab-content">
-                Are you thinking about running a Marathon or just noticed you'd like to be able to take the stairs
+                <strong>FITNESS</strong>Are you thinking about running a Marathon or just noticed you'd like to be able to take the stairs
                 without gasping for air afterwards? No need to search more! We are here for you and together we will
                 make you fit to run stairs so fast you could call yourself The Flash!
             </div>
             <div id="goal-tab-3" class="tab-content">
-                You are already fit and have a healthy lifestyle, but now it's time to define your body or add some
+                <strong>MUSCLE WORK</strong>You are already fit and have a healthy lifestyle, but now it's time to define your body or add some
                 new muscles? This is the place to be! With our Workouts and Nutrition Guides you will be able to
                 build muscles at places you did not know you could even have muscles there!
             </div>
             <div id="goal-tab-4" class="tab-content">
-                Pizza, Burger, Fries and Coke! No thanks, those times are over! If your goal is a healthier
+                <strong>HEALTH</strong> Pizza, Burger, Fries and Coke! No thanks, those times are over! If your goal is a healthier
                 lifestyle, we have Blogs and light Workouts to comfort you in your new and healthy way of living!
                 Your body will love you for it, and you will love the SHAPE OF YOU!
             </div>
         </div>
     </div>
+
+
 
     <div class="home_section home_section_community">
         <div>
@@ -84,7 +81,7 @@
     </div>
 
     <div class="home_section shape_bg">
-        <div>
+        <div class="welcome_shape_content">
             <h2>Choose Your Body Shape</h2>
 
             <ul class="tabs">
@@ -145,7 +142,7 @@
                 What do you think? We think, you should:
             </p>
             @if(auth()->check())
-                <a href="{{ url('superfood') }}">NEWEST BLOG</a>
+                <a href="{{ url('superfood') }}">OUR BLOGS</a>
             @else
                 <a href="{{ url('register') }}">START NOW</a>
             @endif
--- resources/views/workout.blade.php
+++ resources/views/workout.blade.php
@@ -26,17 +26,17 @@
             @if($workout->where('WorkoutCategory', 1)->count() > 0)
                 <div class="box_outdoor">
                     <a href="@if( (auth()->check())){{url('overview/1')}}@else{{ url ('register') }}@endif">
-                        <svg viewbox="0 -1 50 47.4">
+                        <svg viewbox="0 -3 45 45.5">
                             <defs>
                                 <mask class="mask" id="warmup_mask" x="0" y="0" width="220" height="215">
-                                    <rect x="0.5" y="0.5" width="49" height="49" fill="#fff"/>
-                                    <text x="14" text-anchor="middle" y="25" dy="0">W</text>
-                                    <text x="22" text-anchor="middle" y="25" dy="0">A</text>
-                                    <text x="28" text-anchor="middle" y="25" dy="0">R</text>
-                                    <text x="36" text-anchor="middle" y="25" dy="0">M</text>
+                                    <rect x="0.5" y="0.5" width="44" height="41" fill="#fff"/>
+                                    <text x="11" text-anchor="middle" y="25" dy="0">W</text>
+                                    <text x="18" text-anchor="middle" y="25" dy="0">A</text>
+                                    <text x="24" text-anchor="middle" y="25" dy="0">R</text>
+                                    <text x="32" text-anchor="middle" y="25" dy="0">M</text>
 
-                                    <text x="22" text-anchor="middle" y="34" dy="0">U</text>
-                                    <text x="28" text-anchor="middle" y="34" dy="0">P</text>
+                                    <text x="20" text-anchor="middle" y="34" dy="0">U</text>
+                                    <text x="26" text-anchor="middle" y="34" dy="0">P</text>
                                 </mask>
                             </defs>
                             <rect x="0.5" y="0.5" width="220" height="215" mask="url(#warmup_mask)" fill-opacity="1"
@@ -48,17 +48,17 @@
             @if($workout->where('WorkoutCategory', 2)->count() > 0)
                 <div class="box_stretch">
                     <a href="@if( (auth()->check())){{url('overview/2')}}@else{{ url ('register') }}@endif">
-                        <svg viewbox="0 -1 50 47">
+                        <svg viewbox="0 -3 45 45.5">
                             <defs>
                                 <mask class="mask" id="stretch_mask" x="0" y="0" width="220" height="215">
-                                    <rect x="0.5" y="0.5" width="49" height="49" fill="#fff"/>
-                                    <text x="6" text-anchor="middle" y="30" dy="0">S</text>
-                                    <text x="12" text-anchor="middle" y="30" dy="0">T</text>
-                                    <text x="18" text-anchor="middle" y="30" dy="0">R</text>
-                                    <text x="24" text-anchor="middle" y="30" dy="0">E</text>
-                                    <text x="30" text-anchor="middle" y="30" dy="0">T</text>
-                                    <text x="37" text-anchor="middle" y="30" dy="0">C</text>
-                                    <text x="44" text-anchor="middle" y="30" dy="0">H</text>
+                                    <rect x="0.5" y="0.5" width="44" height="41" fill="#fff"/>
+                                    <text x="4" text-anchor="middle" y="30" dy="0">S</text>
+                                    <text x="10" text-anchor="middle" y="30" dy="0">T</text>
+                                    <text x="16" text-anchor="middle" y="30" dy="0">R</text>
+                                    <text x="22" text-anchor="middle" y="30" dy="0">E</text>
+                                    <text x="28" text-anchor="middle" y="30" dy="0">T</text>
+                                    <text x="34" text-anchor="middle" y="30" dy="0">C</text>
+                                    <text x="41" text-anchor="middle" y="30" dy="0">H</text>
                                 </mask>
                             </defs>
                             <rect x="0.5" y="0.5" width="220" height="215" mask="url(#stretch_mask)" fill-opacity="1"
@@ -70,23 +70,23 @@
             @if($workout->where('WorkoutCategory', 3)->count() > 0)
                 <div class="box_indoor">
                     <a href="@if( (auth()->check())){{url('overview/3')}}@else{{ url ('register') }}@endif">
-                        <svg viewbox="0 -1 50 47">
+                        <svg viewbox="0 -3 45 45.5">
                             <defs>
                                 <mask id="mainworkout_mask" x="0" y="0" width="220" height="205">
-                                    <rect x="0.5" y="0.5" width="49" height="49" fill="#fff"/>
+                                    <rect x="0.5" y="0.5" width="44" height="41" fill="#fff"/>
                                     <text x="15" text-anchor="middle" y="24" dy="0">M</text>
                                     <text x="23" text-anchor="middle" y="24" dy="0">A</text>
                                     <text x="27" text-anchor="middle" y="24" dy="0">I</text>
                                     <text x="32" text-anchor="middle" y="24" dy="0">N</text>
 
 
-                                    <text x="6" text-anchor="middle" y="35" dy="0">W</text>
-                                    <text x="14" text-anchor="middle" y="35" dy="0">O</text>
-                                    <text x="21" text-anchor="middle" y="35" dy="0">R</text>
-                                    <text x="27" id="ltrV" text-anchor="middle" y="35" dy="0">K</text>
-                                    <text x="33" text-anchor="middle" y="35" dy="0">O</text>
-                                    <text x="40" text-anchor="middle" y="35" dy="0">U</text>
-                                    <text x="46" text-anchor="middle" y="35" dy="0">T</text>
+                                    <text x="4.5" text-anchor="middle" y="35" dy="0">W</text>
+                                    <text x="11.5" text-anchor="middle" y="35" dy="0">O</text>
+                                    <text x="17.5" text-anchor="middle" y="35" dy="0">R</text>
+                                    <text x="23.5" id="ltrV" text-anchor="middle" y="35" dy="0">K</text>
+                                    <text x="29" text-anchor="middle" y="35" dy="0">O</text>
+                                    <text x="35.5" text-anchor="middle" y="35" dy="0">U</text>
+                                    <text x="41.5" text-anchor="middle" y="35" dy="0">T</text>
                                 </mask>
                             </defs>
                             <rect x="0.5" y="0.5" width="220" height="205" mask="url(#mainworkout_mask)"
